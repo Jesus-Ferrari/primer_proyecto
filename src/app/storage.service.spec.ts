a6fe216983597313597e05d61cd64a70045a7edd
JESUS <je.ferrari@duocuc.cl>
2024-12-04 19:39:36 -0300

    Modificado storage.service.spec.ts segun jasmine

--- src/app/storage.service.spec.ts
+++ src/app/storage.service.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-
 import { StorageService } from './storage.service';
+import { Storage } from '@ionic/storage-angular'; // Ajusta la ruta si es necesario
 
 describe('StorageService', () => {
   let service: StorageService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(StorageService);
+  beforeEach(async () => {
+    // Crear un mock del Storage
+    const storageSpy = jasmine.createSpyObj('Storage', ['create', 'get', 'set', 'remove']);
+
+    await TestBed.configureTestingModule({
+      providers: [
+        StorageService, // Servicio que estamos probando
+        { provide: Storage, useValue: storageSpy } // Mock del Storage
+      ]
+    }).compileComponents();
+
+    service = TestBed.inject(StorageService); // Inyectar el servicio para pruebas
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy(); // Verifica que el servicio fue creado
   });
 });
+
