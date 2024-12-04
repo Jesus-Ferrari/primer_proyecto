b2a17ff1329ec48add180317ea09bb8a308b2e0b
JESUS <je.ferrari@duocuc.cl>
2024-12-04 19:42:37 -0300

    Modificado user.service.spec.ts segun Jasmine

--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -1,16 +1,28 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
+import { StorageService } from './storage.service';
+import { Storage } from '@ionic/storage-angular'; // Ajusta la ruta si es necesario
 
 describe('UserService', () => {
   let service: UserService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+  beforeEach(async () => {
+    // Crear un mock del Storage
+    const storageSpy = jasmine.createSpyObj('Storage', ['create', 'get', 'set', 'remove']);
+
+    await TestBed.configureTestingModule({
+      providers: [
+        UserService, // Servicio que estamos probando
+        StorageService, // Servicio dependiente
+        { provide: Storage, useValue: storageSpy } // Mock del Storage
+      ]
+    }).compileComponents();
+
+    service = TestBed.inject(UserService); // Inyectar el servicio para pruebas
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy(); // Verifica que el servicio fue creado
   });
 });
+
